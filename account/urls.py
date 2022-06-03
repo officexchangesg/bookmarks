fd7c5983ecc460a9baef8760f8829eacade91b3a
officexchangesg <officexchangesg@gmail.com>
2022-06-03 15:17:55 +0800

    withignore

--- account/urls.py
+++ account/urls.py
@@ -34,5 +34,7 @@ urlpatterns = [
 
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+
 
 ]
